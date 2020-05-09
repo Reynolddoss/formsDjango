246070e0944ad7f4ea4853cd58d1fb78d8534464
reynolddoss <reynold.da1@iiitmk.ac.in>
2020-05-09 13:08:10 +0530

    template inheritence

---
+++ forms_project/blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+posts = [
+	{
+		'author':"Mark",
+		'title':"Blog 1",
+		'content':"First blog",
+		'date_posted':"Aug 25 2020"
+	},
+	{
+		'author':"Jude",
+		'title':"Blog 2",
+		'content':"Second blog",
+		'date_posted':"Sep 25 2020"
+	}
+]
+
+def home(request):
+	data = {
+		'posts':posts
+	}
+	return render(request,'blog/home.html',data)
+
+
+def about(request):
+	data = {
+		'title':"About Blog"
+	}
+	return render(request,'blog/about.html',data)
+
