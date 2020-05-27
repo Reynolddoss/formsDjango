dbbf289590f56926550fa2ae6e1251e94c8800af
reynold <reynold.da1@iiitmk.ac.in>
2020-05-27 11:33:57 +0530

    post models

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post 
 
-posts = [
-		{
-			"title":"Blog-1",
-			"author": "Matt",
-			"content":"First blog post",
-			"date_posted":"Jun 01, 2020"
-		},
-		{
-			"title":"Blog-2",
-			"author": "Tony",
-			"content":"Second blog post",
-			"date_posted":"Mar 11, 2020"
-		}
-
-]
 
 def home(request):
 	dummy = {
-	'posts':posts
+	'posts':Post.objects.all()
 	}
 	return render(request,"blog/home.html",dummy)
 
